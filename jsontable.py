db40636ded2d1e677477de184e491ad077018bf2
Alexander Roth <roth.a@mailbox.org>
2020-04-11 22:19:35 +0200

    Added logging

--- jsontable.py
+++ jsontable.py
@@ -1,25 +1,27 @@
 #%%
+import numpy as np
+
 class JsonTable():
     def __init__(self):
         self.table = [[]]
         self.data = {}
-        
+
     def write_table(self, row, column, value):
-        delta_row = len(self.table) - row 
+        delta_row = len(self.table) - row
         if delta_row <= 0:
             self.table += [[] for i in range(-delta_row+1)]
-            
-        delta_column = len(self.table[row]) - column 
+
+        delta_column = len(self.table[row]) - column
         if delta_column <= 0:
             self.table[row] += [None for i in range(-delta_column+1)]
-            
+
         self.table[row][column] = value
-        
-        
-        
+
+
+
     def json2table(self, data):
         self.table = [[]]
-        def rec_json2table(data, row =0, column = 0):        
+        def rec_json2table(data, row =0, column = 0):
             if isinstance(data, dict):
                 for k, v in data.items():
                     self.write_table(row, column, k)
@@ -29,27 +31,30 @@ class JsonTable():
                     self.write_table(row, column, i)
                     row = rec_json2table(v, row, column+1)
             else:
-                self.write_table(row, column, data)  
-                row += 1 
-                
+                self.write_table(row, column, data)
+                row += 1
+
             return row
-        
+
         def rectanglify():
             max_len = max([len(row) for row in self.table])
             for i in range(len(self.table)):
                 self.write_table(i, max_len, None)
-        
+
         rec_json2table(data)
         rectanglify()
-        
+
         return self.table
 
-    def table2json(self, table):        
-        self.data = {}        
+
+
+
+    def table2json(self, table):
+        self.data = {}
         table = np.array(table)
         def rec_table2json(table):
             start_row = 0
-            
+
             key = table[start_row,0]
             if len(table)==1:
                 return table[0,0]
@@ -61,30 +66,31 @@ class JsonTable():
                     data = []
                 else:
                     data = {}
-                    
+
                 def my_append(key, value):
                     if is_list:
                         data.append(value)
                     else:
                         data[key] = value
-                    
-                for i in range(start_row+1, len(table)):                    
+
+                for i in range(start_row+1, len(table)):
                     if not (table[i,0] is None or table[i,0] is ''):
                         my_append(key, rec_table2json(table[start_row:i,1:]))
                         key = table[i,0]
-                        start_row = i                        
+                        start_row = i
                     if i == len(table)-1:
                         my_append(key, rec_table2json(table[start_row:,1:]))
-                        
+
                 return data
-        
-        
+        return rec_table2json(table)
+
+
     def json2mixedtable(self, data):
         """
         Not correctly implemented yet
         """
         self.table = [[]]
-        def rec_json2table(data, row =0, column = 0, vertical = True):        
+        def rec_json2table(data, row =0, column = 0, vertical = True):
             if isinstance(data, dict):
                 initial_row = row
                 for k, v in data.items():
@@ -96,27 +102,26 @@ class JsonTable():
                     self.write_table(row, column, i)
                     row, ___ = rec_json2table(v, row, column+1, vertical=True)
             else:
-                self.write_table(row, column, data)  
+                self.write_table(row, column, data)
                 if vertical:
                     row += 1
                 else:
-                    column += 1 
-                
+                    column += 1
+
             return row, column
-        
+
         def rectanglify():
             max_len = max([len(row) for row in self.table])
             for i in range(len(self.table)):
                 self.write_table(i, max_len, None)
-        
+
         rec_json2table(data)
         rectanglify()
-        
+
         return self.table
 
-        
-        return rec_table2json(table)
-            
+
+
 
 #%%
 # import pprint
@@ -127,12 +132,12 @@ class JsonTable():
 #     j = JsonTable()
 #     w = {'a':0, 'b':0, 'c':[{'a1':0, 'b1':0,},{'a2':0, 'b2':0,}], 'n':[2,3], 'l'}
 #     pp.pprint(w)
-    
+
 #     t = PrettyTable()
 #     for row in j.json2mixedtable(w):
 #         t.add_row(row)
 #     print(t)
-#     # 
+#     #
 #     # with open('output.log', "w") as file:
 #     #     file.write(str(t))
 #     # pp.pprint(j.table2json(j.table))
--- parallel.py
+++ parallel.py
@@ -1,3 +1,17 @@
+import os, sys
+
+import logging
+logger = logging.getLogger(str(os.getpid()) +'."'+__file__+'"')
+# check if there are parents handlers. If not then add console output
+if len(logging.getLogger(str(os.getpid())).handlers) == 0:
+	logger.setLevel(logging.DEBUG)
+	fh = logging.StreamHandler(sys.stdout)
+	fh.setLevel(logging.DEBUG)
+	logger.addHandler(fh)
+logger.debug('Loaded '+ __file__)
+
+
+
 from multiprocessing import Pool
 from multiprocessing import cpu_count
 from concurrent.futures.thread import ThreadPoolExecutor
@@ -6,17 +20,18 @@ import time
 
 
 def ptable(f, arglist, processes=cpu_count()):
-    try:
-        print ('starting the pool map')
-        pool = Pool(processes=processes)  
-        result = pool.map(f, arglist)  
-        return result
-        print ('pool map complete')
-    finally:
-        print ('joining pool processes')
-        pool.close()
-        pool.join()
-        print ('join complete')
+	logger.debug('Starting {} processes {}({})'.format(processes, str(f), str(arglist)))
+	try:
+		print ('starting the pool map')
+		pool = Pool(processes=processes)
+		result = pool.map(f, arglist)
+		return result
+		print ('pool map complete')
+	finally:
+		print ('joining pool processes')
+		pool.close()
+		pool.join()
+		print ('join complete')
 
 
 
@@ -26,26 +41,27 @@ def ptable(f, arglist, processes=cpu_count()):
 
 def threadtable(f, arglist, max_workers=8):
 	with ThreadPoolExecutor(max_workers=max_workers) as executor:
-	    res =  []
-	    for arg in arglist:
-	        res.append(executor.submit(f, arg))
+		logger.debug('Starting {} threads {}({})'.format(max_workers, str(f), str(arglist)))
+		res =  []
+		for arg in arglist:
+			res.append(executor.submit(f, arg))
 	return [r.result() for r in res]
-	
-	
+
+
 #
 #def call_script(ordinal, arg):
-#    print('Thread', ordinal, 'argument:', arg)
-#    time.sleep(2)
-#    print('Thread', ordinal, 'Finished')
-#    return arg
+#	print('Thread', ordinal, 'argument:', arg)
+#	time.sleep(2)
+#	print('Thread', ordinal, 'Finished')
+#	return arg
 
 # tests
-		
+
 #def  f(x):
-#    res= 1
-#    for i in range(1000000):
-#        res=res* x/(x+1)
-#    return res		
+#	res= 1
+#	for i in range(1000000):
+#		res=res* x/(x+1)
+#	return res
 #
 #
-#print(ptable(f, range(30), processes=10))     
+#print(ptable(f, range(30), processes=10))
