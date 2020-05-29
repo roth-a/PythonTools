77105788b95371988389582b7090f574b95c82a3
Alexander Roth <roth.a@mailbox.org>
2020-05-29 17:29:49 +0200

    Removed decorator that did not work well with threads

--- parallel.py
+++ parallel.py
@@ -55,36 +55,7 @@ def threadtable(f, arglist, max_workers=8):
 
 
 
-def rate_limited(max_per_second):
-	"""
-		This is a rate limiter from https://gist.github.com/gregburek/1441055
 
-		This can be combined with threadtable
-	"""
-	lock = threading.Lock()
-	min_interval = 1.0 / max_per_second
-
-	def decorate(func):
-		last_time_called = time.perf_counter()
-
-		@wraps(func)
-		def rate_limited_function(*args, **kwargs):
-			lock.acquire()
-			nonlocal last_time_called
-			elapsed = time.perf_counter() - last_time_called
-			left_to_wait = min_interval - elapsed
-
-			if left_to_wait > 0:
-				time.sleep(left_to_wait)
-
-			ret = func(*args, **kwargs)
-			last_time_called = time.perf_counter()
-			lock.release()
-			return ret
-
-		return rate_limited_function
-
-	return decorate
 
 
 #
