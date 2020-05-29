2804399b780c6071780e9185398b6d6004e5df29
Alexander Roth <roth.a@mailbox.org>
2020-05-29 21:38:37 +0200

    Added rate_limited_threadtable

--- parallel.py
+++ parallel.py
@@ -15,10 +15,7 @@ logger.debug('Loaded '+ __file__)
 from multiprocessing import Pool
 from multiprocessing import cpu_count
 from concurrent.futures.thread import ThreadPoolExecutor
-
-import threading
 import time
-from functools import wraps
 
 
 
@@ -50,6 +47,40 @@ def threadtable(f, arglist, max_workers=8):
 			res.append(executor.submit(f, arg))
 	return [r.result() for r in res]
 
+
+
+
+
+
+
+def rate_limited_threadtable(f, arglist, rate_limit=30):
+	"""
+	It guarantees that less than rate_limit processes run at the same time
+	AND no more than rate_limit/second processes were started.
+
+
+	You can test the function with this code:
+		def f(x):
+		 	print('Start {}'.format(x))
+		 	time.sleep(2)
+
+		start = time.time()
+		arglist=range(30)
+		res = rate_limited_threadtable(f, arglist, rate_limit=15)
+		print(time.time()- start)
+	"""
+	min_duration = 1  # each process needs to take exactly 1 second
+	def rate_limited_f(*args, **kwargs):
+		start = time.time()
+		ret = f(*args, **kwargs)
+
+		duration = time.time() - start
+		if duration < min_duration:
+			print('Sleeping {}s'.format(min_duration - duration))
+			time.sleep(min_duration - duration)
+		return ret
+
+	return threadtable(rate_limited_f, arglist, max_workers=rate_limit)
 
 
 
