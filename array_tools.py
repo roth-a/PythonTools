fd0c8b3540f1ca01dbb2bec36a6078128a9d64dd
Alexander Roth <roth.a@mailbox.org>
2020-09-13 21:44:34 +0200

    Ensured that none of the original None values is replaced

--- array_tools.py
+++ array_tools.py
@@ -12,13 +12,12 @@ def merge_2d_arrays(a, b, b_pos, empty_cell=""):
 		b_pos = [5,5]
 	"""
 	a_v = np.empty( [ max(0,b_pos[0] - a.shape[0] + b.shape[0])  , a.shape[1] ]  ,dtype=object)
-	a_left = np.vstack([a, a_v])
 	a_right = np.empty( [ b_pos[0] + b.shape[0]  , max(0,b_pos[1] - a.shape[1] + b.shape[1]) ] ,dtype=object )
 
 	a_v[a_v == None] = empty_cell
-	a_left[a_left == None] = empty_cell
 	a_right[a_right == None] = empty_cell
 
+	a_left = np.vstack([a, a_v])
 	if min(a_right.shape) != 0:
 		full = np.hstack([a_left, a_right])
 	else:
