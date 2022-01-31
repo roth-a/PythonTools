28b15677f51035c38e55bde017d3e3c4d09454b6
roth-a <roth-a@gmx.de>
2022-01-31 19:25:57 +0100

    Added options for stdout

--- shell.py
+++ shell.py
@@ -14,7 +14,7 @@ logger.debug('Loaded '+ __file__)
 
 import subprocess, shlex
 
-def run_subprocess_cmd(cmd):
+def run_subprocess_cmd(cmd, shell=False, print_stdout=True):
 	"""
 	Runs a subprocess and returns the result as a CompletedProcess type
 
@@ -33,5 +33,7 @@ def run_subprocess_cmd(cmd):
 			- stderr
 			- stdout
 	"""
-	result = subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+	result = subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=shell)
+	if print_stdout:
+		print(result.stdout)
 	return result
